250d3b6a4d3edd9dc54bf95d94bf5baa1c46b1b6
Jamshid93 <hamzayevjf@gmail.com>
2019-07-15 00:36:55 +0500

    find methid output index  at letter

--- StrMethod.py
+++ StrMethod.py
@@ -1,3 +1,3 @@
 course=" Python for Beginners "
 # print(len(course)) # output is " 22 " because course have 22 latters and spaces
-print(course.upper())
+print(course.upper()) # upper method wrting with big latters all of input information
